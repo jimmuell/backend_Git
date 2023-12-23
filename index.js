2932509971bba970b7e5097f2806526320a377b7
James L Mueller <jamesmueller@liongatetechnology.com>
2023-12-23 09:29:25 -0600

    added middleware

--- index.js
+++ index.js
@@ -7,9 +7,18 @@ const port = 3000;
 require('dotenv/config');
 const api = process.env.API_URL;
 
+//middleware for express to parse the body of the request
+//this replaces body-parser
+app.use(express.json());
+
 //http://localhost:3000/api/v1/products
-app.get(api + '/', (req, res) => {
-    res.send('Hello World!');
+app.get(api + '/products', (req, res) => {
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some url'
+    };
+    res.send(product);
 });
 
 app.listen(port, () => {
